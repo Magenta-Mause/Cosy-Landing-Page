02ff73fe4f74530c844a7f7ff5621ff670cbf1fd
Alexander Jablonowski <alexander@jablonowski.dev>
2026-03-24 08:32:42 +0100

    feat: update cosy version

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const DISCORD_INVITE_URL = "https://discord.gg/t2zFuPT6";
 export const GITHUB_REPO_URL = "https://github.com/Magenta-Mause/Cosy";
 
-export const COSY_VERSION = "v1.0.0";
+export const COSY_VERSION = "v1.0.1";
 export const INSTALL_COMMAND = `sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/Magenta-Mause/Cosy/${COSY_VERSION}/install_cosy.sh)" _`;
 export const UNINSTALL_DOCKER_COMMAND = `sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/Magenta-Mause/Cosy/${COSY_VERSION}/uninstall_cosy.sh)" _ docker`;
 export const UNINSTALL_KUBERNETES_COMMAND = `sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/Magenta-Mause/Cosy/${COSY_VERSION}/uninstall_cosy.sh)" _ kubernetes`;
